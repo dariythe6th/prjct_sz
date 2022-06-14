4e65e174856c051576e8b5a4d0ce8dbe8a05bffa
Dariy the 6th <66427137+dariythe6th@users.noreply.github.com>
2022-06-14 16:37:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ def check(a):
             return b
     return ''
 
+
 class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -71,12 +72,14 @@ class Users(Base):
     time_g_end = Column(String)
     users_shares = relationship("Sleep")
 
+
 class Admin(Base):
     __tablename__ = 'admin'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     name_id = Column(String)
     idt = Column(Integer)
+
 
 class Sleep(Base):
     __tablename__ = "sleep"
@@ -86,7 +89,9 @@ class Sleep(Base):
     date = Column(String)
     rate = Column(Integer)
     k = Column(Integer)
+    points = Column(Integer)
     user_id = Column(Integer, ForeignKey("users.id"))
+
 
 class Tips(Base):
     __tablename__ = 'tips'
@@ -105,6 +110,7 @@ def on_start(update, context):
     context.bot.send_message(chat_id=chat.id,
                              text="Привет, я твой помощник для сна. \nДля начала напиши во сколько ты хочешь ложиться и вставать под #цель\nЧтобы я смогу помочь тебе со сном утром под #утро пиши когда ты проснулся, во сколько лёг, ну и опиши свое состояние по шкале от 1 до 10")
 
+
 def add(s, newansw):
     answ = s.split('\n')
     for i in range(int(len(answ))):
@@ -117,6 +123,7 @@ def add(s, newansw):
     newansw[1] = check(newansw[1])
     return newansw
 
+
 def prepare():
     conn = sqlite3.connect('sz.db')
     users = pd.read_sql("select * from users", conn)
@@ -126,6 +133,9 @@ def prepare():
     sleep['date'] = pd.to_datetime(sleep['date'], format='%Y-%m-%d')
     sleep['time_start'] = pd.to_timedelta(sleep['time_start'])
     sleep['time_end'] = pd.to_timedelta(sleep['time_end'])
+    sleep['k'] = sleep['k'].fillna(0)
+    sleep['k'] = sleep['k'].astype('float')
+    sleep['points'] = sleep['points'].fillna(0)
     pd.options.mode.chained_assignment = None
     ref_time = time.fromisoformat('17:30:00')
     for i in range(sleep.shape[0]):
@@ -133,6 +143,51 @@ def prepare():
         if real_time > ref_time:
             sleep['time_start'][i] -= pd.Timedelta(days=1)
     return sleep, users
+
+
+def prepare2(person_data):
+    person_data['target_time_start'] = person_data['date'] + person_data['target_time_start']
+    person_data['target_time_end'] = person_data['date'] + person_data['target_time_end']
+    start = person_data['time_start'] + person_data['date']
+    end = person_data['time_end'] + person_data['date']
+    person_data['ttdiff_start'] = abs(person_data['target_time_start'] - start).astype('timedelta64[m]')
+    person_data['ttdiff_end'] = abs(pd.Timedelta(days=0) - (person_data['target_time_end'] - end)).astype(
+        'timedelta64[m]')
+    return person_data
+
+
+def sleep_k(diff_series, ind):
+    groups = pd.qcut(range(15), 181, labels=False)
+    if diff_series[ind] <= 180:
+        for i in range(len(groups)):
+            if diff_series[ind] < groups[i]:
+                sleep_k = ((
+                                       16 - i) * 0.02333)  # 0.04666 (*15 = 0.7) бонус поделили на 2, тк общий бонус состоит из 2 частей (подъем и отход ко сну)
+                break
+    return sleep_k
+
+
+def strike_beginning(date_ser):
+    beg_day = date_ser[date_ser.shape[0] - 1]
+    for i in range(date_ser.shape[0] - 2, -1, -1):
+        prev_day = date_ser[i]
+        if (beg_day - prev_day) > pd.Timedelta(days=1):
+            return i + 1
+        else:
+            beg_day = prev_day
+    return 0
+
+
+def check_k(person_data):
+    for i in range(len(person_data['date'])):
+        strike_days = 1
+        if i >= 1:  # если до этого уже были дни с записями о прочтении отчетов, нужно учесть страйк
+            last_strike_beg = strike_beginning(
+                person_data['date'])  # последний день, когда отчета не было: со следующего считаем страйк дней
+            strike_days = 1 * len(person_data['date'][last_strike_beg:i + 1])
+            if (strike_days > 10): strike_days = 10  # максимальный страйк -- 10 дней
+    return strike_days * 0.03
+
 
 def smth(users, person_data, x):
     ind = users[users['id'] == x].index[0]
@@ -144,6 +199,7 @@ def smth(users, person_data, x):
         person_data['target_time_start'] = tts
     person_data['target_time_end'] = pd.to_timedelta(users[users['id'] == x]['time_g_end'][ind])
     return person_data
+
 
 def onesleep(x):
     sleep, users = prepare()
@@ -200,6 +256,7 @@ def onesleep(x):
     )
     pio.write_image(fig_sleep, r"fig.jpeg")
 
+
 def ratesleep(x):
     sleep, users = prepare()
     fig_rates = go.Figure()
@@ -235,6 +292,7 @@ def ratesleep(x):
                            range=[0, 10], )
     pio.write_image(fig_rates, r"fig.jpeg")
 
+
 def on_message(update, context):
     chat = update.effective_chat
     text = update.message.text
@@ -248,13 +306,13 @@ def on_message(update, context):
             context.bot.delete_message(chat_id=chat.id,
                                        message_id=update.message.message_id)
             a1 = Admin(name=Name, name_id=id_n, idt=id_t)
-            ms = session.query(Admin).filter(Admin.idt==id_t).first()
+            ms = session.query(Admin).filter(Admin.idt == id_t).first()
             if ms == None:
                 session.add(a1)
                 context.bot.send_message(chat_id=chat.id, text="Я вас запомнил, новый админ")
-        elif text[0] == "-" and session.query(Admin).filter(Admin.idt==id_t).first()!=None:
-            if text[1:7] =="график":
-                idch=text[8::]
+        elif text[0] == "-" and session.query(Admin).filter(Admin.idt == id_t).first() != None:
+            if text[1:7] == "график":
+                idch = text[8::]
                 ms = session.query(Users).filter(Users.name_id == idch).first()
                 if ms == None:
                     context.bot.send_message(chat_id=chat.id, text="Такого пользователя нет")
@@ -294,10 +352,22 @@ def on_message(update, context):
                                                  1] + ", а оценил на " + newansw[2])
                 print("DO")
                 session.commit()
+            sleep, users = prepare()
+            person_data = sleep[sleep['user_id'] == ms.id]
+            person_data = person_data.drop_duplicates(subset=['date'])
+            person_data = smth(users, person_data, ms.id)
+            person_data = prepare2(person_data)
+            if pd.Timestamp('now').normalize() in person_data['date'].unique():
+                person_current_k = person_data[person_data['date'] == pd.Timestamp('now').normalize()]
+                ind = person_current_k.index[0]
+                person_current_k['k'] = sleep_k(person_current_k['ttdiff_start'], ind) + sleep_k(
+                    person_current_k['ttdiff_end'], ind) + 1
+                session.query(Sleep).filter(Sleep.idt == id_t).update({'k': person_current_k['k'][ind] + check_k()})
+                session.query(Sleep).filter(Sleep.idt == id_t).update({'points': 15 * person_data['k'][ind]})
         if s[1:7] == 'график':
             id_n = update.message.from_user.username
             with sessionmaker(bind=engine).begin() as session:
-                ms = session.query(Users).filter(Users.idt==id_t).first()
+                ms = session.query(Users).filter(Users.idt == id_t).first()
                 if ms == None:
                     context.bot.send_message(chat_id=chat.id, text="Введи сначала цель под #цель")
                 else:
@@ -313,20 +383,20 @@ def on_message(update, context):
             id_t = update.message.from_user.id
             u1 = Users(name=Name, name_id=id_n, idt=id_t, time_g_start=newansw[0], time_g_end=newansw[1])
             with sessionmaker(bind=engine).begin() as session:
-                ms = session.query(Users).filter(Users.idt==id_t).first()
+                ms = session.query(Users).filter(Users.name_id == id_n).first()
                 if ms == None:
                     session.add(u1)
                     context.bot.send_message(chat_id=chat.id, text="Данные записаны")
                 else:
 
-                    session.query(Users).filter(Users.idt==id_t).update({'time_g_start': newansw[0]})
-                    session.query(Users).filter(Users.idt==id_t).update({'time_g_end': newansw[1]})
+                    session.query(Users).filter(Users.name_id == id_n).update({'time_g_start': newansw[0]})
+                    session.query(Users).filter(Users.name_id == id_n).update({'time_g_end': newansw[1]})
                     context.bot.send_message(chat_id=chat.id, text="Данные обновлены")
             session.commit()
         if s[1:7] == 'оценка':
             id_n = update.message.from_user.username
             with sessionmaker(bind=engine).begin() as session:
-                ms = session.query(Users).filter(Users.idt==id_t).first()
+                ms = session.query(Users).filter(Users.idt == id_t).first()
                 if ms == None:
                     context.bot.send_message(chat_id=chat.id, text="Введи сначала цель под #цель")
                 else:
